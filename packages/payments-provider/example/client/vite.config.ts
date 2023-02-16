b92bace20bda52cf0b2f9ce250404761be6e9055
Yago Pérez Vázquez <yago@safe.global>
2023-02-16 13:06:16 +0100

    Use port 300 instead vite default one

--- packages/payments-provider/example/client/vite.config.ts
+++ packages/payments-provider/example/client/vite.config.ts
@@ -5,6 +5,9 @@ import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfil
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000
+  },
   optimizeDeps: {
     disabled: false,
     esbuildOptions: {

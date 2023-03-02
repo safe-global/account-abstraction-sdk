8109969aa62988b8bf5709eb19cbed0d31ba823b
Yago Pérez Vázquez <yago@safe.global>
2023-03-02 17:52:49 +0100

    Add tests for events

--- packages/onramp-kit/src/adapters/stripe/StripeAdapter.test.ts
+++ packages/onramp-kit/src/adapters/stripe/StripeAdapter.test.ts
@@ -1,7 +1,7 @@
+import EventEmitter from 'events'
 import { SafeOnRampOpenOptions, Session } from '../../types'
 import { StripeAdapter } from './StripeAdapter'
 import * as stripeApi from './stripeApi'
-
 import * as utils from './utils'
 
 const openOptions: SafeOnRampOpenOptions = {
@@ -35,13 +35,19 @@ const session: Session = {
   }
 }
 
+const eventEmitter = new EventEmitter()
 const mockMount = jest.fn()
-const mockEventListener = jest.fn()
+const mockAddEventListener = jest
+  .fn()
+  .mockImplementation((event, listener) => eventEmitter.on(event, listener))
+const mockDispatch = jest.fn().mockImplementation((event, data) => eventEmitter.emit(event, data))
+
 global.StripeOnramp = jest.fn().mockImplementation(() => {
   return {
     createSession: jest.fn().mockResolvedValue({
       mount: mockMount,
-      addEventListener: mockEventListener
+      addEventListener: mockAddEventListener,
+      dispatchEvent: mockDispatch
     })
   }
 })
@@ -81,7 +87,9 @@ describe('StripeAdapter', () => {
     jest.spyOn(utils, 'loadScript').mockImplementation(() => {
       return Promise.resolve()
     })
-    jest.spyOn(stripeApi, 'createSession').mockImplementationOnce(() => Promise.resolve(session))
+    const createSessionSpy = jest
+      .spyOn(stripeApi, 'createSession')
+      .mockImplementationOnce(() => Promise.resolve(session))
 
     const stripeAdapter = new StripeAdapter(config)
 
@@ -91,5 +99,92 @@ describe('StripeAdapter', () => {
 
     expect(mockMount).toHaveBeenCalledWith(openOptions.element)
     expect(returnedSession).toEqual(session)
+    expect(createSessionSpy).toHaveBeenCalledWith('https://onramp-backend-url', {
+      walletAddress: '0x',
+      networks: ['ethereum']
+    })
+  })
+
+  it('should throw an exception if the createSession fail', async () => {
+    jest.spyOn(utils, 'loadScript').mockImplementation(() => {
+      return Promise.resolve()
+    })
+    jest.spyOn(stripeApi, 'createSession').mockImplementationOnce(() => Promise.reject())
+
+    const stripeAdapter = new StripeAdapter(config)
+
+    await stripeAdapter.init()
+
+    await expect(stripeAdapter.open(openOptions)).rejects.toThrow(
+      'Error trying to create a new Stripe session'
+    )
+  })
+
+  it('should try to get the session if a sessionId is provided', async () => {
+    jest.spyOn(utils, 'loadScript').mockImplementation(() => {
+      return Promise.resolve()
+    })
+    const getSessionSpy = jest
+      .spyOn(stripeApi, 'getSession')
+      .mockImplementationOnce(() => Promise.resolve(session))
+
+    const stripeAdapter = new StripeAdapter(config)
+
+    await stripeAdapter.init()
+
+    const returnedSession = await stripeAdapter.open({ ...openOptions, sessionId: 'session-id' })
+
+    expect(mockMount).toHaveBeenCalledWith(openOptions.element)
+    expect(returnedSession).toEqual(session)
+    expect(getSessionSpy).toHaveBeenCalledWith('https://onramp-backend-url', 'session-id')
+  })
+
+  it('should respond to events', async () => {
+    const mockOnLoaded = jest.fn()
+    const mockOnPaymentSuccessful = jest.fn()
+    const mockOnPaymentProcessing = jest.fn()
+    const mockOnPaymentError = jest.fn()
+
+    jest.spyOn(utils, 'loadScript').mockImplementation(() => {
+      return Promise.resolve()
+    })
+    jest.spyOn(stripeApi, 'createSession').mockImplementationOnce(() => Promise.resolve(session))
+
+    const stripeAdapter = new StripeAdapter(config)
+
+    await stripeAdapter.init()
+
+    await stripeAdapter.open({
+      ...openOptions,
+      events: {
+        onLoaded: mockOnLoaded,
+        onPaymentSuccessful: mockOnPaymentSuccessful,
+        onPaymentProcessing: mockOnPaymentProcessing,
+        onPaymentError: mockOnPaymentError
+      }
+    })
+
+    expect(mockAddEventListener).toHaveBeenCalledTimes(2)
+    mockDispatch('onramp_ui_loaded', 'sessionData')
+    expect(mockOnLoaded).toHaveBeenCalled()
+
+    mockDispatch('onramp_session_updated', {
+      payload: {
+        session: { status: 'fulfillment_complete', quote: { source_monetary_amount: '10' } }
+      }
+    })
+    expect(mockOnPaymentSuccessful).toHaveBeenCalled()
+
+    mockDispatch('onramp_session_updated', {
+      payload: {
+        session: { status: 'fulfillment_processing', quote: { source_monetary_amount: '10' } }
+      }
+    })
+    expect(mockOnPaymentProcessing).toHaveBeenCalled()
+
+    mockDispatch('onramp_session_updated', {
+      payload: { session: { status: 'rejected', quote: { source_monetary_amount: '10' } } }
+    })
+    expect(mockOnPaymentError).toHaveBeenCalled()
   })
 })

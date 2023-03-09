f857c6b57208ce6b1a725cf84689dbb4f93e5413
Yago Pérez Vázquez <yago@safe.global>
2023-03-09 12:47:43 +0100

    Add new createRelayedTransaction tests

--- packages/relay-kit/src/GelatoRelayAdapter.test.ts
+++ packages/relay-kit/src/GelatoRelayAdapter.test.ts
@@ -1,13 +1,17 @@
 import { BigNumber } from '@ethersproject/bignumber'
 import { TransactionStatusResponse } from '@gelatonetwork/relay-sdk'
-import { GELATO_FEE_COLLECTOR, GELATO_NATIVE_TOKEN_ADDRESS } from './constants'
+import { GELATO_FEE_COLLECTOR, GELATO_NATIVE_TOKEN_ADDRESS, ZERO_ADDRESS } from './constants'
 import { GelatoRelayAdapter } from './GelatoRelayAdapter'
+import Safe from '@safe-global/safe-core-sdk'
+import * as safeCoreSdk from '@safe-global/safe-core-sdk'
+import { OperationType } from '@safe-global/safe-core-sdk-types'
 
 enum TaskState {
   CheckPending = 'CheckPending'
 }
 
 const CHAIN_ID = 1
+const ADDRESS = '0x...address'
 const SAFE_ADDRESS = '0x...safe-address'
 const API_KEY = 'api-key'
 const FEE_ESTIMATION = BigNumber.from(100000)
@@ -21,6 +25,19 @@ const TASK_STATUS: TransactionStatusResponse = {
 const RELAY_RESPONSE = {
   taskId: TASK_ID
 }
+const SAFE_TRANSACTION = {
+  operation: OperationType.Call,
+  safeTxGas: 0,
+  baseGas: 0,
+  gasPrice: 0,
+  nonce: 0,
+  gasToken: '0x',
+  refundReceiver: '0x',
+  to: ADDRESS,
+  value: '0',
+  data: '0x'
+}
+
 const mockGetEstimateFee = jest.fn().mockResolvedValue(FEE_ESTIMATION)
 const mockGetTaskStatus = jest.fn().mockResolvedValue(TASK_STATUS)
 const mockSponsoredCall = jest.fn().mockResolvedValue(RELAY_RESPONSE)
@@ -39,9 +56,15 @@ jest.mock('@gelatonetwork/relay-sdk', () => {
   }
 })
 
+jest.mock('@safe-global/safe-core-sdk')
+
 const gelatoRelayAdapter = new GelatoRelayAdapter(API_KEY)
 
 describe('GelatoRelayAdapter', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
   it('should allow to get a fee estimation', async () => {
     const chainId = 1
     const gasLimit = BigNumber.from(100000)
@@ -85,6 +108,79 @@ describe('GelatoRelayAdapter', () => {
     await expect(
       relayAdapter.sponsorTransaction(SAFE_ADDRESS, '0x', CHAIN_ID)
     ).rejects.toThrowError('API key not defined')
+  })
+
+  describe('when creating a relayed transaction', () => {
+    let relayAdapter: GelatoRelayAdapter
+    const safe: Safe = new Safe()
+    const args: any = [
+      {
+        to: ADDRESS,
+        data: '0x',
+        value: '0'
+      },
+      safe,
+      {
+        gasLimit: BigNumber.from(100),
+        isSponsored: true
+      }
+    ]
+
+    beforeEach(() => {
+      jest.clearAllMocks()
+      relayAdapter = new GelatoRelayAdapter()
+      safe.getNonce = jest.fn().mockResolvedValue(0)
+      safe.getContractManager = jest.fn().mockReturnValue({ safeContract: {} })
+      jest.spyOn(safeCoreSdk, 'standardizeSafeTransactionData').mockResolvedValue(SAFE_TRANSACTION)
+    })
+
+    it('should allow you to create an sponsored one', async () => {
+      await relayAdapter.createRelayedTransaction(args[0], args[1], args[2])
+
+      expect(safeCoreSdk.standardizeSafeTransactionData).toHaveBeenCalledWith(
+        {},
+        undefined,
+        expect.objectContaining({
+          baseGas: 0,
+          gasPrice: 0,
+          refundReceiver: ZERO_ADDRESS
+        })
+      )
+    })
+
+    it('should allow to create a payed one', async () => {
+      await relayAdapter.createRelayedTransaction(args[0], args[1], {
+        ...args[2],
+        isSponsored: false
+      })
+
+      expect(safeCoreSdk.standardizeSafeTransactionData).toHaveBeenCalledWith(
+        {},
+        undefined,
+        expect.objectContaining({
+          baseGas: 100000,
+          gasPrice: 1,
+          refundReceiver: GELATO_FEE_COLLECTOR
+        })
+      )
+    })
+
+    it('should return the correct gasToken when being sent through the options', async () => {
+      const GAS_TOKEN = '0x...gasToken'
+
+      await relayAdapter.createRelayedTransaction(args[0], args[1], {
+        ...args[2],
+        gasToken: GAS_TOKEN
+      })
+
+      expect(safeCoreSdk.standardizeSafeTransactionData).toHaveBeenCalledWith(
+        {},
+        undefined,
+        expect.objectContaining({
+          gasToken: GAS_TOKEN
+        })
+      )
+    })
   })
 
   it('should allow to make a payed transaction', async () => {

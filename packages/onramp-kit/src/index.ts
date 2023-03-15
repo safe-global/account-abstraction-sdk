3708781d45943228eb034df4ff0ec0f6020d6a7d
Yago Pérez Vázquez <yago@safe.global>
2023-03-15 23:01:57 +0100

    fix import

--- packages/onramp-kit/src/index.ts
+++ packages/onramp-kit/src/index.ts
@@ -1,5 +1,7 @@
 export * from './packs/stripe/StripeAdapter'
 export * from './packs/stripe/types'
+export * from './packs/coinbase/CoinbaseAdapter'
+export * from './packs/coinbase/types'
 
 export * from './SafeOnRampKit'
 export * from './types'
